926b11d050482239cc4ef0a4bf882f2229465814
Talal Mahmud <45840662+Talalmahmud@users.noreply.github.com>
2022-07-30 09:53:19 +0600

    Create scope.py

---
+++ scope.py
@@ -0,0 +1,22 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+        self.maximumDifference = 0
+
+    # Add your code here
+    def computeDifference(self):
+        max_value = max(self.__elements)
+        min_value = min(self.__elements)
+        
+        self.maximumDifference = max_value - min_value
+                
+
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
