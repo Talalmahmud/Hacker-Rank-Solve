1e9198faac1b85b8b3072a10721c2e4e97eba90b
Talal Mahmud <45840662+Talalmahmud@users.noreply.github.com>
2022-08-20 10:01:51 +0600

    Create utopain_tree.py

---
+++ utopain_tree.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    # Write your code here
+    h = 1
+    c = 0
+    if n == 0:
+        return 1
+    for i in range(1,n+1):
+        if c == 2:
+            h *= 2
+            c = 0
+        else:
+            c += 1
+            h += 1
+    return h
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
