65a019dedc33c9a85c6b0448611e35dbb530d864
Talal Mahmud <45840662+Talalmahmud@users.noreply.github.com>
2022-08-21 23:22:24 +0600

    Create find_digit.py

---
+++ find_digit.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    # Write your code here
+    c = 0
+    t=n
+    while(n!=0):
+        m = n%10
+        n = n//10
+        if m!=0:
+           if t%m == 0:
+               c += 1
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
