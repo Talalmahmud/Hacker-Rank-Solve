7ce2ac505183844fa209238bc8dddf475ad40039
Talal Mahmud <45840662+Talalmahmud@users.noreply.github.com>
2022-08-22 17:57:32 +0600

    Time limit problem

---
+++ strange_counter.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    # Write your code here
+    values = 4
+    d = 3
+    for i in range(1,t+1):
+        if values == 1:
+            d = d*2
+            values = d
+        else:
+            values -= 1
+    return values
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
