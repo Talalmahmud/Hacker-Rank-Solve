fa6e8126a221b9ed127006d106a2e77064184269
Talal Mahmud <45840662+Talalmahmud@users.noreply.github.com>
2022-08-30 10:39:12 +0600

    Create migratory_birds.py

---
+++ migratory_birds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    a = []
+    b = {}
+    for i in arr:
+        if i not in a:
+            b[i] = arr.count(i)
+            
+    max_v = max(b,key=b.get)
+    return max_v
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
